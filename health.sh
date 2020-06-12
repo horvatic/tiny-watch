1771389406c8843d0062aea5c20fb82e597f4f96
horvatic <sbhorvatic@gmail.com>
2020-06-12 00:17:01 -0400

    Update health.sh

--- health.sh
+++ health.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 
-landscape-sysinfo | jq -sR '[sub("\n$";"") | splits("\n") | sub("^:[ \t]+";"") | [splits(":[ \t]+")]]' | jq '.[] | {"type": .[0], "status": .[1]}' | sed 's/" */"/g'
+landscape-sysinfo | jq -sR '[sub("\n$";"") | splits("\n") | sub("^:[ \t]+";"") | [splits(":[ \t]+")]]' | jq '[.[] | {"type": .[0], "status": .[1]}]' | sed 's/" */"/g'
  
